2717f5099cb55a0b1adc7fb32e99297659ce94b9
Joel Yek <joevo2@gmail.com>
2016-05-01 20:38:22 +0800

    implemented 2 box and both can move togher

--- src/Tutorial.java
+++ src/Tutorial.java
@@ -12,6 +12,7 @@ import java.awt.event.KeyListener;
  */
 public class Tutorial extends JPanel implements ActionListener, KeyListener{
     int x = 0, y = 0, velX = 0, velY = 0;
+    int x1 = 100, y1 = 100, velX1 = 0, velY1 = 0;
     Timer tm = new Timer(5, this);
 
     public Tutorial() {
@@ -25,10 +26,14 @@ public class Tutorial extends JPanel implements ActionListener, KeyListener{
         super.paintComponent(g);
         g.setColor(Color.RED);
         g.fillRect(x, y, 50, 30);
+
+        g.setColor(Color.BLUE);
+        g.fillRect(x1, y1, 50, 30);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        // Red box
         if (x < 0) {
             velX = 0;
             x = 0;
@@ -51,6 +56,30 @@ public class Tutorial extends JPanel implements ActionListener, KeyListener{
 
         x = x + velX;
         y = y + velY;
+
+        // Blue box
+        if (x1 < 0) {
+            velX1 = 0;
+            x1 = 0;
+        }
+
+        if (x1 > 530) {
+            velX1 = 0;
+            x1 = 530;
+        }
+
+        if (y1 < 0) {
+            velY1 = 0;
+            y1 = 0;
+        }
+
+        if (y1 > 330) {
+            velY1 = 0;
+            y1 = 330;
+        }
+
+        x1 = x1 + velX1;
+        y1 = y1 + velY1;
         repaint();
     }
 
@@ -63,6 +92,7 @@ public class Tutorial extends JPanel implements ActionListener, KeyListener{
     public void keyPressed(KeyEvent e) {
         int c = e.getKeyCode();
 
+        // Red box
         if (c == KeyEvent.VK_LEFT) {
             velX = -1;
             velY = 0;
@@ -79,12 +109,33 @@ public class Tutorial extends JPanel implements ActionListener, KeyListener{
             velX = 0;
             velY = 1;
         }
+
+        // Blue box
+        if (c == KeyEvent.VK_A) {
+            velX1 = -1;
+            velY1 = 0;
+        }
+        if (c == KeyEvent.VK_W) {
+            velX1 = 0;
+            velY1 = -1;
+        }
+        if (c == KeyEvent.VK_D) {
+            velX1 = 1;
+            velY1 = 0;
+        }
+        if (c == KeyEvent.VK_S) {
+            velX1 = 0;
+            velY1 = 1;
+        }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
         velX = 0;
         velY = 0;
+
+        velX1 = 0;
+        velY1 = 0;
     }
 
     public static void main(String[] args) {
